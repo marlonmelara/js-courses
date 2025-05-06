7042b064829997b1b5b72467173803024daf3ea8
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2025-05-05 22:05:24 -0400

    Docs: Add definitions and characteristics of pure and impure functions

--- 04-platzi/01-js-desde-cero/20-funciones-puras-e-impuras.js
+++ 04-platzi/01-js-desde-cero/20-funciones-puras-e-impuras.js
@@ -9,6 +9,13 @@
 // 6. Obtener la hora actual
 
 // Función pura
+// Siempre devuelve el mismo resultado para los mismos argumentos.
+// No tiene efectos secundarios (no modifica variables externas, no realiza operaciones de E/S, etc.).
+/** Características:
+- Determinísticas: Su salida depende únicamente de sus entradas.
+- No interactúan con el estado externo (variables globales, DOM, etc.).
+*/
+
 function sum(a, b) {
   return a + b;
 }
@@ -17,6 +24,13 @@ const sumarAyB = sum(5, 1);
 console.log(sumarAyB);
 
 // Función impura
+// No tiene efectos secundarios (no modifica variables externas, no realiza operaciones de E/S, etc.).
+// Tiene efectos secundarios (modifica variables externas, realiza operaciones de E/S, etc.).
+/** Características:
+- No son determinísticas: Su salida puede depender de factores externos.
+- Pueden interactuar con el estado externo.
+*/
+
 function sum(a, b) {
   console.log("A: ", a);
   return a + b;
@@ -25,7 +39,7 @@ function sum(a, b) {
 // Función impura
 let total = 0;
 function sumWithSideEffect(a) {
-  total += a;
+  total += a; // Modifica una variable externa
   return total;
 }
 

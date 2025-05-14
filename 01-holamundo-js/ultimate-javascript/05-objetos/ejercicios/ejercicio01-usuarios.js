ad5bfc5cab749ba8c62585bad95e521f8c4a1406
Marlon Melara <110437919+marlonmelara@users.noreply.github.com>
2025-05-14 17:17:10 -0400

    ✨ [ADD] user creation examples using constructor function and class
    - Implemented a  constructor function to create user objects with properties like name, age, and address.
    - Added a modern  class to demonstrate the same functionality with ES6 syntax.
    - Created instances of users with fictional data based on Peanuts characters.
    - Included examples of printing user objects to the console.

---
+++ 01-holamundo-js/ultimate-javascript/05-objetos/ejercicios/ejercicio01-usuarios.js
@@ -0,0 +1,114 @@
+// Ejercicio 1: Crear un objeto de usuario
+
+// Función constructora para crear un objeto de usuario
+function User(
+  name,
+  lastname,
+  birthdate,
+  address,
+  age,
+  countryOfBirth,
+  isSuscriber
+) {
+  this.name = name;
+  this.lastname = lastname;
+  this.birthdate = birthdate;
+  this.address = address;
+  this.age = age;
+  this.countryOfBirth = countryOfBirth;
+  this.isSuscriber = isSuscriber;
+}
+
+// Instancias de usuarios con datos ficticios basados en Peanuts
+const user1 = new User(
+  "Charlie",
+  "Brown",
+  "30 de octubre de 1950",
+  "Calle Principal 1",
+  74,
+  "Estados Unidos",
+  true
+);
+
+const user2 = new User(
+  "Lucy",
+  "van Pelt",
+  "3 de marzo de 1952",
+  "Av. del Psiquiatra 5",
+  72,
+  "Estados Unidos",
+  false
+);
+
+const user3 = new User(
+  "Snoopy",
+  "Dog",
+  "4 de octubre de 1950",
+  "Casa Roja en el Jardín",
+  74,
+  "Estados Unidos",
+  true
+);
+
+// Imprimir los objetos de usuario en la consola
+console.log(user1);
+console.log(user2);
+console.log(user3);
+
+// Script moderna con class
+// Clase Usuario con propiedades definidas en el constructor
+class Usuario {
+  constructor(
+    nombre,
+    apellido,
+    fechaNacimiento,
+    direccion,
+    edad,
+    paisNacimiento,
+    suscripcionActiva
+  ) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.fechaNacimiento = fechaNacimiento;
+    this.direccion = direccion;
+    this.edad = edad;
+    this.paisNacimiento = paisNacimiento;
+    this.suscripcionActiva = suscripcionActiva;
+  }
+}
+
+// Instancias de usuarios con datos ficticios basados en Peanuts
+const usuario1 = new Usuario(
+  "Charlie",
+  "Brown",
+  "30 de octubre de 1950",
+  "Calle Principal 1",
+  74,
+  "Estados Unidos",
+  true
+);
+
+const usuario2 = new Usuario(
+  "Lucy",
+  "van Pelt",
+  "3 de marzo de 1952",
+  "Av. del Psiquiatra 5",
+  72,
+  "Estados Unidos",
+  false
+);
+
+const usuario3 = new Usuario(
+  "Snoopy",
+  "Dog",
+  "4 de octubre de 1950",
+  "Casa Roja en el Jardín",
+  74,
+  "Estados Unidos",
+  true
+);
+
+// Mostrar usuarios en consola
+console.log(usuario1);
+console.log(usuario2);
+console.log(usuario3);
